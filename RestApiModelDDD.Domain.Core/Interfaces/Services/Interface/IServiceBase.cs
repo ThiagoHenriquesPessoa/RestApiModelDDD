ed98a2382893c533ac81e6c8c72bdbc67734122b
Thiago Henriques Pessoa <thiagokxl5@gmail.com>
2022-04-20 23:06:15 -0300

    Interface de servico

---
+++ RestApiModelDDD.Domain.Core/Interfaces/Services/Interface/IServiceBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace RestApiModelDDD.Domain.Core.Interfaces.Services.Interface
+{
+    public interface IServiceBase<T> where T : class
+    {
+        void Add(T obj);
+
+        void Update(T obj);
+
+        void Remove(T obj);
+
+        IEnumerable<T> GetAll();
+
+        T GetById(int id);
+    }
+}
---
+++ RestApiModelDDD.Domain.Core/Interfaces/Services/Interface/IServiceProduto.cs
@@ -0,0 +1,8 @@
+﻿using RestApiModelDDD.Domain.Entitys;
+
+namespace RestApiModelDDD.Domain.Core.Interfaces.Services.Interface
+{
+    public interface IServiceProduto : IServiceBase<Produto>
+    {
+    }
+}

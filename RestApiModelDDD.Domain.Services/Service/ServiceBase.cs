b3bce0f99d3a1dc4b9443dc2302630ed13d56207
Thiago Henriques Pessoa <thiagokxl5@gmail.com>
2022-04-21 11:08:38 -0300

    Service

---
+++ RestApiModelDDD.Domain.Services/Service/ServiceBase.cs
@@ -0,0 +1,41 @@
+﻿using RestApiModelDDD.Domain.Core.Interfaces.Repositorys;
+using RestApiModelDDD.Domain.Core.Interfaces.Services.Interface;
+using System.Collections.Generic;
+
+namespace RestApiModelDDD.Domain.Services.Service
+{
+    public class ServiceBase<T> : IServiceBase<T> where T : class
+    {
+        private readonly IRepositoryBase<T> _repository;
+
+        public ServiceBase(IRepositoryBase<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public void Add(T obj)
+        {
+            _repository.Add(obj);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public T GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public void Remove(T obj)
+        {
+            _repository.Remove(obj);
+        }
+
+        public void Update(T obj)
+        {
+            _repository.Update(obj);
+        }
+    }
+}
---
+++ RestApiModelDDD.Domain.Services/Service/ServiceProduto.cs
@@ -0,0 +1,16 @@
+﻿using RestApiModelDDD.Domain.Core.Interfaces.Repositorys;
+using RestApiModelDDD.Domain.Core.Interfaces.Services.Interface;
+using RestApiModelDDD.Domain.Entitys;
+
+namespace RestApiModelDDD.Domain.Services.Service
+{
+    public class ServiceProduto : ServiceBase<Produto>, IServiceProduto
+    {
+        private readonly IRepositoryProduto _repositoryProduto;
+
+        public ServiceProduto(IRepositoryProduto repositoryProduto) : base(repositoryProduto)
+        {
+            _repositoryProduto = repositoryProduto;
+        }
+    }
+}

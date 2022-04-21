62c191bcab3142a5dc4b28aaf180662908948d6b
Thiago Henriques Pessoa <thiagokxl5@gmail.com>
2022-04-21 12:44:12 -0300

    Aplication service

--- RestApiModelDDD.Application/Entitys/AplicationServiceCliente.cs
+++ RestApiModelDDD.Application/Entitys/AplicationServiceCliente.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RestApiModelDDD.Application.Dtos;
+using RestApiModelDDD.Application.Interface;
+using RestApiModelDDD.Domain.Core.Interfaces.Services.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,32 @@ using System.Threading.Tasks;
 
 namespace RestApiModelDDD.Application.Entitys
 {
-    class AplicationServiceCliente
+    public class AplicationServiceCliente : IAplicationServiceCliente
     {
+        private readonly IServiceCliente _serviceCliente;
+        public void Add(ClienteDto clienteDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ClienteDto> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClienteDto GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(ClienteDto clienteDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ClienteDto clienteDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

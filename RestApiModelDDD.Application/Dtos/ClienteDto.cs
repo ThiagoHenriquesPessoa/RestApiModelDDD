5c33d04162aa0074ab922e1fe3d09bd0673b9a97
Thiago Henriques Pessoa <thiagokxl5@gmail.com>
2022-04-21 11:14:43 -0300

    Dtos

---
+++ RestApiModelDDD.Application/Dtos/ClienteDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RestApiModelDDD.Application.Dtos
+{
+    public class ClienteDto
+    {
+        public int? Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+    }
+}

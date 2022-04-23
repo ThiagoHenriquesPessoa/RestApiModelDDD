ef2e9f0a852baf40afa9f6e85365731ee08a8861
Thiago Henriques Pessoa <thiagokxl5@gmail.com>
2022-04-23 11:47:24 -0300

    Controlleres

---
+++ RestApiModelDDD.API/Controllers/ClienteController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestApiModelDDD.Application.Dtos;
+using RestApiModelDDD.Application.Interface;
+using System;
+using System.Collections.Generic;
+
+namespace RestApiModelDDD.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ClienteController : Controller
+    {
+        private readonly IAplicationServiceCliente _aplicationServiceCliente;
+
+        public ClienteController(IAplicationServiceCliente aplicationServiceCliente)
+        {
+            _aplicationServiceCliente = aplicationServiceCliente;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_aplicationServiceCliente.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return Ok(_aplicationServiceCliente.GetById(id));
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] ClienteDto clienteDto)
+        {
+            try
+            {
+                if (clienteDto == null) return NotFound();
+                _aplicationServiceCliente.Add(clienteDto);
+                return Ok("Cliente cadastrado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] ClienteDto clienteDto)
+        {
+            try
+            {
+                if (clienteDto == null) return NotFound();
+                _aplicationServiceCliente.Update(clienteDto);
+                return Ok("Cliente atualizado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Delete([FromBody] ClienteDto clienteDto)
+        {
+            try
+            {
+                if (clienteDto == null) return NotFound();
+                _aplicationServiceCliente.Remove(clienteDto);
+                return Ok("Cliente removido com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ RestApiModelDDD.API/Controllers/ProdutoController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestApiModelDDD.Application.Dtos;
+using RestApiModelDDD.Application.Interface;
+using System;
+using System.Collections.Generic;
+
+namespace RestApiModelDDD.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutoController : Controller
+    {
+        private readonly IAplicationServiceProduto _aplicationServiceProduto;
+
+        public ProdutoController(IAplicationServiceProduto aplicationServiceProduto)
+        {
+            _aplicationServiceProduto = aplicationServiceProduto;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_aplicationServiceProduto.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return Ok(_aplicationServiceProduto.GetById(id));
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] ProdutoDto produtoDto)
+        {
+            try
+            {
+                if (produtoDto == null) return NotFound();
+                _aplicationServiceProduto.Add(produtoDto);
+                return Ok("Cliente cadastrado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] ProdutoDto produtoDto)
+        {
+            try
+            {
+                if (produtoDto == null) return NotFound();
+                _aplicationServiceProduto.Update(produtoDto);
+                return Ok("Cliente atualizado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Delete([FromBody] ProdutoDto produtoDto)
+        {
+            try
+            {
+                if (produtoDto == null) return NotFound();
+                _aplicationServiceProduto.Remove(produtoDto);
+                return Ok("Cliente removido com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
